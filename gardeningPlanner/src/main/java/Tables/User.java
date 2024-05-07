d2e0473df1093ee9375048c6cc970d3249d83676
CarolineRoemer1998 <caroroemer@gmx.de>
2024-05-07 12:23:44 +0200

    Added Entity Annotation
    Co-authored-by: Helmni <120278094+Helmni@users.noreply.github.com>
    Co-authored-by: KonradSmyk <wgc312@haw-hamburg.de>
    Co-authored-by: marinazoe <marina.koops55@googlemail.com>

--- gardeningPlanner/src/main/java/Tables/User.java
+++ gardeningPlanner/src/main/java/Tables/User.java
@@ -3,11 +3,13 @@ package Tables;
 import java.util.List;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- gardeningPlanner/src/main/java/Tables/UserPlant.java
+++ gardeningPlanner/src/main/java/Tables/UserPlant.java
@@ -1,12 +1,14 @@
 package Tables;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class UserPlant {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
